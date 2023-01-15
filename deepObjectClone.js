cda3e1e92dc991e3cbebc2e8f4cf929b543c3a15
Barroso Filho <barrosofilho.ti@gmail.com>
2023-01-14 22:50:10 -0300

    Check for cyclic reference

--- deepObjectClone.js
+++ deepObjectClone.js
@@ -1,14 +1,16 @@
-function deepClone(obj) {
+function deepClone(obj, hash = new WeakMap()) {
     if (typeof obj !== "object" || obj === null) {
         return obj;
     }
+    // Check for cyclic reference
+    if (hash.has(obj)) return hash.get(obj);
 
     let clone = Array.isArray(obj) ? [] : {};
+    hash.set(obj, clone);
     for (const key in obj) {
         if (obj.hasOwnProperty(key)) {
-            clone[key] = deepClone(obj[key]);
+            clone[key] = deepClone(obj[key], hash);
         }
     }
-
     return clone;
 }
